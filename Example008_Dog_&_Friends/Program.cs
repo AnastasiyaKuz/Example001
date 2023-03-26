e07ca3dc08138e9ba2c7117cef9419d085e1bf99
Anastasiya <kuznecovaas94@mail.ru>
2023-03-27 00:43:38 +0300

    Example  008 доработка

--- Example008_Dog_&_Friends/Program.cs
+++ Example008_Dog_&_Friends/Program.cs
@@ -1,21 +1,22 @@
-﻿int time;        
+﻿string time = Console.ReadLine();
+int num1 = Convert.ToInt32(time); 
+string distance = Console.ReadLine();
+int num2 = Convert.ToInt32(distance);        
 int distance = 10000, FirstFriendSpeed = 1, 
    SecondFriendSpeed = 2, DogSpeed = 5, friend = 2, count = 0;
 while (distance > 10)
 {
     if (friend == 1)
     {
-    int time, distance;
-    int time = (distance / (FirstFriendSpeed + DogSpeed)) ;
-    Console.WriteLine(time);
-    friend = 2;
+      int time = (distance / (FirstFriendSpeed + DogSpeed)) ;
+      Console.WriteLine(time);
+      friend = 2;
     } 
     else
     {
-    int time, distance;
-    int time = (distance / (SecondFriendSpeed + DogSpeed));
-    Console.WriteLine(time);
-    friend =1;
+      int time = (distance / (SecondFriendSpeed + DogSpeed));
+      Console.WriteLine(time);
+      friend =1;
     }
   int distance = (distance - (FirstFriendSpeed + SecondFriendSpeed) * time);
   Console.WriteLine(distance);
